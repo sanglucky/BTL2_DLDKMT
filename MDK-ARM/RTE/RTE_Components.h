802dadb6ce1b5036650ac7428425bebd5cefdaca
sanglucky <sang.nguyenluckysmart@hcmut.edu.vn>
2024-03-17 17:04:36 +0700

    BLT2

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TUT_DS18B20_MULTI' 
+ * Target:  'TUT_DS18B20_MULTI' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/TUT_DS18B20_MULTI/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+tut_ds18b20_multi\startup_stm32f103xb.o: startup_stm32f103xb.s
